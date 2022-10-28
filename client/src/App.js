cbe0dcef7369fa1cab7c426c2ff6cc4824a863ba
Sunil Kumar <sunilk10099@gmail.com>
2022-10-28 16:36:58 +0530

    create basic homepage template

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,33 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import headerLogo  from "./assets/images/logo.png"
+import useStyles from "./styles"
+
 const App = () => {
-  return <div className="App"></div>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={headerLogo} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-betweeen" alignItems="space-between" spacing={4}>
+              <Grid item xs={12} sm= {8}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm= {4}>
+                <Form />
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
 };
 
 export default App;

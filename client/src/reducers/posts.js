612d4151dfa507cc73a2b2aad4ff8a6480baf6a0
Sunil Kumar <sunilk10099@gmail.com>
2022-11-03 21:26:33 +0530

    add constants to reducers

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,23 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE_POST,
+} from "../constants/actionTypes";
+
 export default (posts = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
-    case "LIKE_POST":
-    case "UPDATE":
+    case LIKE_POST:
+    case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
       return posts;

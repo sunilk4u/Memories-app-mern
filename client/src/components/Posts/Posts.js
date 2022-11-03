eb0c6cb4727dac4b1a003764d746b70a5ab6b512
Sunil Kumar <sunilk10099@gmail.com>
2022-11-03 20:09:15 +0530

    delete console logs

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -8,8 +8,6 @@ const Posts = ({ setCurrentId }) => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
 
-  console.log(posts);
-
   return !posts.length ? (
     <CircularProgress />
   ) : (

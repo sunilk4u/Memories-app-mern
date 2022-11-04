3ff04fd39d167c5e09c58d1184cbaa609f7b4f88
Sunil Kumar <sunilk10099@gmail.com>
2022-11-04 12:23:59 +0530

    create seperate component for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid } from "@material-ui/core";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
-import headerLogo from "./assets/images/logo.png";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
 import { getPosts } from "./actions/posts";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   const classes = useStyles();
@@ -18,17 +18,7 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={headerLogo}
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
+      <Navbar />
       <Grow in>
         <Container>
           <Grid

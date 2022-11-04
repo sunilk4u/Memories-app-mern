525fdc639e49246dcda66b86756d26d6854455ac
Sunil Kumar <sunilk10099@gmail.com>
2022-11-04 14:54:04 +0530

    create form and custom input component

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,92 @@
-import React from 'react'
-import useStyles from "./styles"
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Input from "./Input";
+import useStyles from "./styles";
 
 const Auth = () => {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isSignUp = true;
+
+  //handle form submit
+  const handleSubmit = () => {};
+
+  //handle form value change
+  const handleChange = () => {};
+
+  //handle show password
+  const handleShowPassword = () => setShowPassword((prev) => !prev);
 
   return (
-    <></>
-  )
-}
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={3}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstname"
+                  label="First name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastname"
+                  label="Last name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Confirm Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignUp ? "Sign up": "Sign in"}
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Auth
+export default Auth;

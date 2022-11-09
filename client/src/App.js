f1ec81a353a91e32652cde1d276fee2f93d1604e
Sunil Kumar <sunilk10099@gmail.com>
2022-11-09 18:21:10 +0530

    add routes for search and postdetails

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
   return (
     <Router>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
 
         {/* Routes for the app */}
         <Routes>
-          <Route path="/" exact element={<Home />} />
+          <Route path="/" exact element={<Navigate to="/posts" />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={<PostDetails />} />
+          <Route path="/posts/search" exact element={<Home />} />
           <Route path="/auth" exact element={<Auth />} />
         </Routes>
-      
       </Container>
     </Router>
   );

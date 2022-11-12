706c7185b06964ea0ee086911eba7eb875c77e4d
Sunil Kumar <sunilk10099@gmail.com>
2022-11-12 18:27:38 +0530

    fix edit button propagation

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -85,7 +85,10 @@ const Post = ({ post, setCurrentId }) => {
             <Button
               style={{ color: "white" }}
               size="small"
-              onClick={() => setCurrentId(post._id)}
+              onClick={(e) => {
+                e.stopPropagation();
+                setCurrentId(post._id);
+              }}
             >
               <MoreHorizIcon fontSize="medium" />
             </Button>
